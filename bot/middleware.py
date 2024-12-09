e0d261740d21146ba8f6a56ecd89c61d3935e21b
ph4nt0mm <100.ac@mail.ru>
2024-12-09 13:37:22 +0300

    feat: finish checking subscription

--- bot/middleware.py
+++ bot/middleware.py
@@ -1,6 +1,7 @@
-from aiogram import BaseMiddleware
+from aiogram import BaseMiddleware, types
 
 from aiogram import Bot
+from aiogram.enums import ParseMode
 from aiogram.exceptions import TelegramForbiddenError, TelegramBadRequest
 from aiogram.types import Update, TelegramObject
 from loguru import logger
@@ -9,10 +10,9 @@ from loguru import logger
 # Subscription Middleware
 class SubscriptionMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: TelegramObject, data: dict):
-        if not isinstance(event, Update) or not event.message:  # TODO fix buttons click
-            return await handler(event, data)
+        user_id = event.event.from_user.id
+        message = event.message or event.callback_query.message
 
-        user_id = event.message.from_user.id
         group_id = -1002489680551  # TODO set from settings
         channel_id = -1001765033775  # TODO set from settings
 
@@ -39,9 +39,11 @@ class SubscriptionMiddleware(BaseMiddleware):
         ] or channel_status.status not in [
             'member', 'administrator', 'creator',
         ]):
-            await event.message.answer(
-                f'Please subscribe to our (group)[{...}] '  # TODO set from settings
-                f'and (channel)[{...}] to use the bot.'  # TODO set from settings
+            await message.answer(
+                f'Please subscribe to our [group](https://t.me/jgftdhtes), '
+                f'[channel](https://t.me/HandsOverTheMemes) to use the bot\n'
+                f'And try again',
+                parse_mode=ParseMode.MARKDOWN
             )
             return
         return await handler(event, data)
